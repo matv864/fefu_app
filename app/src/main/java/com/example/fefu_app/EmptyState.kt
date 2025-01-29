a7c70e00e6877d10c28510349fffbda17a9b3153
matv864 <matv864@gmail.com>
2025-01-30 08:56:41 +1000

    some time and I'll finish 3rd task

--- app/src/main/java/com/example/fefu_app/EmptyState.kt
+++ app/src/main/java/com/example/fefu_app/EmptyState.kt
@@ -1,11 +1,16 @@
 package com.example.fefu_app
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import com.example.fefu_app.fragment.ActivityFragment
+import com.example.fefu_app.fragment.ProfileFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class EmptyState : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -14,8 +19,50 @@ class EmptyState : AppCompatActivity() {
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                 .add(R.id.fragment_container, ActivityFragment(), "ActivityFragment")
+                .add(R.id.fragment_container, ProfileFragment(), "ProfileFragment")
+                .commit()
+            switchFragment(activities_is_selected = true)
+            Log.e("tag", "it's really null")
+        }
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        bottomNavigationView.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.navigation_activity -> {
+                    switchFragment(activities_is_selected = true)
+                    true
+                }
+                R.id.navigation_profile -> {
+                    switchFragment(activities_is_selected = false)
+                    true
+                }
+                else -> false
+            }
+        }
+
+    }
+
+    private fun switchFragment(activities_is_selected: Boolean) {
+        var activities_fragment: Fragment? = supportFragmentManager.findFragmentByTag("ActivityFragment")
+        var profile_fragment: Fragment? = supportFragmentManager.findFragmentByTag("ProfileFragment")
+
+        if (activities_fragment == null || profile_fragment == null) {
+            // check only for kotlin type checking
+            return
+        }
+
+        if (activities_is_selected) {
+            supportFragmentManager.beginTransaction()
+                .show(activities_fragment)
+                .hide(profile_fragment)
+                .commit()
+        } else {
+            supportFragmentManager.beginTransaction()
+                .show(profile_fragment)
+                .hide(activities_fragment)
                 .commit()
         }
+
 
     }
 }
