35d5f3b4ff123621f54e427ef1d88e1991fc543f
matv864 <matv864@gmail.com>
2025-01-30 00:44:55 +1000

    save

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,7 +39,6 @@ android {
 }
 
 dependencies {
-
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
--- app/src/main/java/com/example/fefu_app/Registration.kt
+++ app/src/main/java/com/example/fefu_app/Registration.kt
@@ -1,14 +1,26 @@
 package com.example.fefu_app
 
 import android.os.Bundle
+import android.widget.FrameLayout
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 
 
 class Registration : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.registration)
+
+        (findViewById<FrameLayout>(R.id.nested_bar)
+                    .findViewById<TextView>(R.id.title)
+                    .setText(getString(R.string.registration_page))
+                )
+
+
+//        titleInBar = getView
+
     }
 }
