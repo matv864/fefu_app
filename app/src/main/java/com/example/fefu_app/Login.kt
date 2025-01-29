f7a455b4d97a2f798f3bd77fe5cc399274281b92
matv864 <matv864@gmail.com>
2025-01-30 03:55:57 +1000

    end task 3 activity

--- app/src/main/java/com/example/fefu_app/Login.kt
+++ app/src/main/java/com/example/fefu_app/Login.kt
@@ -1,14 +1,32 @@
 package com.example.fefu_app
 
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageButton
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 
-class Login : AppCompatActivity() {
+
+class Login : AppCompatActivity(), View.OnClickListener {
+    lateinit var backButton: ImageButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.login)
 
+        findViewById<TextView>(R.id.title)
+            .setText(getString(R.string.login_page))
+
+        backButton = findViewById(R.id.back_button)
+        backButton.setOnClickListener(this)
+
+    }
+
+    override fun onClick(view: View) {
+        if (view.id == R.id.back_button) {
+            finish()
+        }
     }
 }
--- app/src/main/java/com/example/fefu_app/Registration.kt
+++ app/src/main/java/com/example/fefu_app/Registration.kt
@@ -1,26 +1,33 @@
 package com.example.fefu_app
 
 import android.os.Bundle
-import android.widget.FrameLayout
+import android.view.View
+import android.widget.ImageButton
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 
 
-class Registration : AppCompatActivity() {
+class Registration : AppCompatActivity(), View.OnClickListener {
+    lateinit var backButton: ImageButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.registration)
 
-        (findViewById<FrameLayout>(R.id.nested_bar)
-                    .findViewById<TextView>(R.id.title)
-                    .setText(getString(R.string.registration_page))
-                )
+        findViewById<TextView>(R.id.title)
+            .setText(getString(R.string.registration_page))
+
+        backButton = findViewById(R.id.back_button)
+        backButton.setOnClickListener(this)
+
+    }
 
 
-//        titleInBar = getView
-
+    override fun onClick(view: View) {
+        if (view.id == R.id.back_button) {
+            finish()
+        }
     }
 }
